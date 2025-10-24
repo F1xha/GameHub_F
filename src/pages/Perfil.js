a5710f0051105ac48a256efeeb1b2a70e6c1e4b4
f1xha <an.esquerre@duocuc.cl>
2025-10-23 21:25:59 -0300

    Se agregan mas pages con su codigo

---
+++ src/pages/Perfil.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Perfil(){
+  return (
+    <div>
+      <h1>Mi Perfil</h1>
+      <p>Detalles del usuario (cuando haya sesión/autenticación).</p>
+    </div>
+  );
+}
---
+++ src/pages/SobreNosotros.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SobreNosotros(){
+  return (
+    <div>
+      <h1>Sobre Nosotros</h1>
+      <p>Información sobre la empresa, misión y visión.</p>
+    </div>
+  );
+}
